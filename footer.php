357aa0bca5d307e7e2e68ec9cb82c7cbd33be289
nmn08 <60121529+nmn08@users.noreply.github.com>
2020-09-28 13:49:53 -0700

    Footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php
+echo 
+"<footer class=\"navbar fixed-bottom mb-3\">
+    <div class=\"container justify-content-center\">
+        <a href=\"https://www.linkedin.com/in/minh-nguyen-5b2a901a2\" class=\"btn btn-primary active\" role=\"button\">Linkedin</a>
+    </div>
+</footer>";
+?>
